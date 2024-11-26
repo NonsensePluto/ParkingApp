84c74f2c10a5121b04617c83ad389d71d60a84ea
NonsencePluto <kirill210804@mail.ru>
2024-11-26 15:34:40 +0300

    1

---
+++ Parking/Parking.Service/IoC/DbContextConfigurator.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Parking.DataAccess;
+
+namespace Parking.IoC;
+
+
+public class DbContextConfigurator
+{
+    public static void ConfigureServices(WebApplicationBuilder builder)
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", false)
+            .Build();
+        var connectionString = configuration.GetValue<string>("EventPosterDbContext");
+        builder.Services.AddDbContextFactory<ParkingDbContext>(
+            options => { options.UseNpgsql(connectionString); },
+            ServiceLifetime.Scoped);
+    }
+
+    public static void ConfigureApplication(IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        var contextFactory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<EventPosterDbContext>>();
+        using var context = contextFactory.CreateDbContext();
+        context.Database.Migrate();
+    }
+}
